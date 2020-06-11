43627da064df576be4d992146f2bf8e955d6f086
kaidornel <kai_dornel@hotmail.com>
2020-06-11 10:18:14 -0400

    styled home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,21 +1,38 @@
 import React from 'react'
-import {Button} from 'semantic-ui-react'
+import {Button, Grid} from 'semantic-ui-react'
 import {Link} from 'react-router-dom'
 
 const Home = (props) => {
     return(
         <div>
-            <h1>Home</h1>
-            <div>
-                <h1>Access thousands of online courses today!</h1>
-                <Link to="/course-list"><Button>Browse Courses</Button></Link>
+            <br></br><br></br>
+            <div className="home-div">
+                <div className="top-learning-div">
+                    <img className="top-learning-image" src="../../../images/Online-education-laptop.jpg"></img>
+                    <div className="inside-top-learning-image">
+                        <br></br><br></br>
+                        <h1 className="inside-top-learning-image-h1">Access thousands of online courses today!</h1>
+                        <Link to="/course-list" className="inside-top-learning-image-h1"><Button>Browse Courses</Button></Link>
+                    </div>
+                </div>
+            <br></br><br></br><br></br><br></br><br></br>
+            <div className="what-you-can-do-div">
+                <Grid>
+                    <Grid.Column width={4}>
+                        <h4>Expert Instruction</h4>
+                    </Grid.Column>
+                    <Grid.Column width={4}>
+                        <h4>Further Your Education</h4>
+                    </Grid.Column>
+                    <Grid.Column width={4}>
+                        <h4>Learn New Skills</h4>
+                    </Grid.Column>
+                    <Grid.Column width={4}>
+                        <h4>Earn Certificates</h4>
+                    </Grid.Column>
+                </Grid>
             </div>
-            <br></br>
-            <h2>Expert Instruction</h2>
-            <h2>Further Your Education</h2>
-            <h2>Learn New Skills</h2>
-            <h2>Earn Certificates by Completing Courses</h2>
-
+            </div>
             <br></br><br></br><br></br><br></br><br></br><br></br>
             <div>
                 <h2>Popular Subjects</h2>
