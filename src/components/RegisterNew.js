0aa0e2eeaec94ab4c1289a2a9af0e045b733f403
kaidornel <kai_dornel@hotmail.com>
2020-06-17 00:11:33 -0400

    can create new user with facial recognition

--- src/components/RegisterNew.js
+++ src/components/RegisterNew.js
@@ -3,11 +3,7 @@ import {Radio, Grid, Form, Header, Input, Segment, Image, Button} from 'semantic
 import {Link} from 'react-router-dom'
 import { connect } from "react-redux";
 import {creatingNewUser, creatingNewCompany} from '../redux/actions'
-import Webcam from 'react-webcam';
-
-const WIDTH = 420;
-const HEIGHT = 420;
-const inputSize = 160;
+import NewUserVideoInput from '../views/NewUserVideoInput';
 
 class RegisterNew extends React.Component{
     constructor(){
@@ -17,49 +13,19 @@ class RegisterNew extends React.Component{
             email: "",
             password: "",
             user_or_company: null,
-            clickedNext: false, 
-            face: []
+            clickedNext: false,
+            descriptors: []
         }
     }
 
-    componentWillMount = async () => {
+    componentWillMount(){
         this.setState({
             name: "",
             email: "",
             password: "",
             user_or_company: null
         })
-        // await loadModels();
-        this.setInputDevice();
     }
-
-    setInputDevice = () => {
-        navigator.mediaDevices.enumerateDevices().then(async devices => {
-          let inputDevice = await devices.filter(
-            device => device.kind === 'videoinput'
-          );
-          if (inputDevice.length < 2) {
-            await this.setState({
-              facingMode: 'user'
-            });
-          } else {
-            await this.setState({
-              facingMode: { exact: 'environment' }
-            });
-          }
-        //   this.startCapture();
-        });
-      };
-    
-    //   startCapture = () => {
-    //     this.interval = setInterval(() => {
-    //       this.capture();
-    //     }, 1500);
-    //   };
-    
-    //   componentWillUnmount() {
-    //     clearInterval(this.interval);
-    //   }
 
     signUp = (event) => {
         console.log(event.target.id)
@@ -77,6 +43,8 @@ class RegisterNew extends React.Component{
     handleSubmit = () => {
         if(this.state.name === "" || this.state.email === "" || this.state.password === ""){
             alert("Invalid entry. Please enter a valid email, name, and password")
+        }else if(this.state.descriptors === "" && this.state.user_or_company === "user"){
+            alert("It appears you have not give our system enough time to analyze your face. Please try again")
         }else{
             if(this.state.user_or_company === "user"){
                 this.props.creatingNewUser(this.state)
@@ -86,69 +54,16 @@ class RegisterNew extends React.Component{
         }
     }
 
-    // setState({state},() => {
-    //     other stuff here
-    // })
+    getDescriptors = (descriptors) => {
+        // debugger
+        this.setState({descriptors: descriptors})
+    }
 
     next = () => {
         this.setState({ clickedNext: !this.state.clickedNext })
     }
 
     render(){
-        const { detections, match, facingMode } = this.state;
-            let videoConstraints = null;
-            let camera = '';
-            if (!!facingMode) {
-            videoConstraints = {
-                width: WIDTH,
-                height: HEIGHT,
-                facingMode: facingMode
-            };
-            if (facingMode === 'user') {
-                camera = 'Front';
-            } else {
-                camera = 'Back';
-            }
-        }
-        let drawBox = null;
-        if (!!detections) {
-          drawBox = detections.map((detection, i) => {
-            let _H = detection.box.height;
-            let _W = detection.box.width;
-            let _X = detection.box._x;
-            let _Y = detection.box._y;
-            return (
-              <div key={i}>
-                <div
-                  style={{
-                    position: 'absolute',
-                    border: 'solid',
-                    borderColor: 'blue',
-                    height: _H,
-                    width: _W,
-                    transform: `translate(${_X}px,${_Y}px)`
-                  }}
-                >
-                  {!!match && !!match[i] ? (
-                    <p
-                      style={{
-                        backgroundColor: 'blue',
-                        border: 'solid',
-                        borderColor: 'blue',
-                        width: _W,
-                        marginTop: 0,
-                        color: '#fff',
-                        transform: `translate(-3px,${_H}px)`
-                      }}
-                    >
-                      {match[i]._label}
-                    </p>
-                  ) : null}
-                </div>
-              </div>
-            );
-          });
-        }
         return(
             <div className="user-signup"> 
                 <Grid textAlign='center' style={{ height: '93vh'}} verticalAlign='middle' >
@@ -200,41 +115,16 @@ class RegisterNew extends React.Component{
                             />
                         </Form.Field>
                         {/* <Link to ={"/login"}> */}
-                            <Button type="submit">Next</Button>
+                            <Button type="submit">Image Capture</Button>
                             {this.state.clickedNext ? 
-                           <div
-                           className="Camera"
-                           style={{
-                             display: 'flex',
-                             flexDirection: 'column',
-                             alignItems: 'center'
-                           }}
-                         >
-                           {/* <p>Camera: {camera}</p> */}
-                           <h3></h3>
-                           <div
-                             style={{
-                               width: WIDTH,
-                               height: HEIGHT
-                             }}
-                           >
-                             <div style={{ position: 'relative', width: WIDTH }}>
-                               {!!videoConstraints ? (
-                                 <div style={{ position: 'absolute' }}>
-                                   <Webcam
-                                     audio={false}
-                                     width={WIDTH}
-                                     height={HEIGHT}
-                                     ref={this.webcam}
-                                     screenshotFormat="image/jpeg"
-                                     videoConstraints={videoConstraints}
-                                   />
-                                 </div>
-                               ) : null}
-                               {!!drawBox ? drawBox : null}
-                             </div>
-                           </div>
-                         </div> : null}
+                                <div>
+                                    <Form onSubmit={this.handleSubmit}>
+                                        <NewUserVideoInput getDescriptors={this.getDescriptors} />
+                                        <br></br>
+                                        <Button type="submit">Register</Button>
+                                    </Form>
+                                </div>
+                            : null}
                         {/* </Link> */}
                         </Segment>
                         </Form>
---
+++ src/views/NewUserVideoInput.js
@@ -0,0 +1,207 @@
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+import Webcam from 'react-webcam';
+import { loadModels, getFullFaceDescription, createMatcher } from '../api/face';
+
+// Import face profile
+const JSON_PROFILE = require('../descriptors/bnk48.json');
+
+const WIDTH = 420;
+const HEIGHT = 420;
+const inputSize = 160;
+
+class VideoInput extends Component {
+  constructor(props) {
+    super(props);
+    this.webcam = React.createRef();
+    this.state = {
+      fullDesc: null,
+      detections: null,
+      descriptors: null,
+      faceMatcher: null,
+      match: null,
+      facingMode: null
+    };
+  }
+
+  componentWillMount = async () => {
+    // let userInfo
+    // const response = await fetch('http://localhost:3000/users')
+    // const users = await response.json()
+    //   for(let i = 0; i < users.length; i++){
+    //     if(users[i].email === this.props.email){
+
+    //       users[i].face = [users[i].face]
+    //       userInfo = users[i]
+          
+
+          await loadModels();
+          //{Me: {…}, Cherprang: {…}}
+          // console.log(JSON_PROFILE)
+          
+          // this.setState({ faceMatcher: await createMatcher(  {userInfo}  ) });
+          // this.setState({ faceMatcher: await createMatcher(  JSON_PROFILE  ) });
+          this.setInputDevice();
+      //   }
+      // }
+  };
+
+
+  setInputDevice = () => {
+    navigator.mediaDevices.enumerateDevices().then(async devices => {
+      let inputDevice = await devices.filter(
+        device => device.kind === 'videoinput'
+      );
+      if (inputDevice.length < 2) {
+        await this.setState({
+          facingMode: 'user'
+        });
+      } else {
+        await this.setState({
+          facingMode: { exact: 'environment' }
+        });
+      }
+      this.startCapture();
+    });
+  };
+
+  startCapture = () => {
+    this.interval = setInterval(() => {
+      this.capture();
+    }, 1500);
+  };
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  capture = async () => {
+    if (!!this.webcam.current) {
+      // 
+      try {
+      console.log(this.webcam.current)
+      await getFullFaceDescription(
+        this.webcam.current.getScreenshot(),
+        inputSize
+      ).then(fullDesc => {
+        if (!!fullDesc) {
+          console.log(fullDesc)
+          // 
+          this.setState({
+            detections: fullDesc.map(fd => fd.detection),
+            descriptors: fullDesc.map(fd => fd.descriptor)
+          });
+        }
+      });
+
+      // if (!!this.state.descriptors && !!this.state.faceMatcher) {
+      //   let match = await this.state.descriptors.map(descriptor => {
+      //     return this.state.faceMatcher.findBestMatch(descriptor)
+      //   })
+        
+        // this.setState({ descriptors });
+        this.props.getDescriptors(this.state.descriptors)
+      // }
+    }
+      catch(e){
+        console.log("caught")
+        this.capture()
+      }
+    }
+  };
+
+  render() {
+    const { detections, match, facingMode } = this.state;
+    let videoConstraints = null;
+    let camera = '';
+    if (!!facingMode) {
+      videoConstraints = {
+        width: WIDTH,
+        height: HEIGHT,
+        facingMode: facingMode
+      };
+      if (facingMode === 'user') {
+        camera = 'Front';
+      } else {
+        camera = 'Back';
+      }
+    }
+
+    let drawBox = null;
+    if (!!detections) {
+      drawBox = detections.map((detection, i) => {
+        let _H = detection.box.height;
+        let _W = detection.box.width;
+        let _X = detection.box._x;
+        let _Y = detection.box._y;
+        return (
+          <div key={i}>
+            <div
+              style={{
+                position: 'absolute',
+                border: 'solid',
+                borderColor: 'blue',
+                height: _H,
+                width: _W,
+                transform: `translate(${_X}px,${_Y}px)`
+              }}
+            >
+              {!!match && !!match[i] ? (
+                <p
+                  style={{
+                    backgroundColor: 'blue',
+                    border: 'solid',
+                    borderColor: 'blue',
+                    width: _W,
+                    marginTop: 0,
+                    color: '#fff',
+                    transform: `translate(-3px,${_H}px)`
+                  }}
+                >
+                  {match[i]._label}
+                </p>
+              ) : null}
+            </div>
+          </div>
+        );
+      });
+    }
+    return (
+      <div
+        className="Camera"
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center'
+        }}
+      >
+        {/* <p>Camera: {camera}</p> */}
+        <h3></h3>
+        <div
+          style={{
+            width: WIDTH,
+            height: HEIGHT
+          }}
+        >
+          <div style={{ position: 'relative', width: WIDTH }}>
+            {!!videoConstraints ? (
+              <div style={{ position: 'absolute' }}>
+                <Webcam
+                  audio={false}
+                  width={WIDTH}
+                  height={HEIGHT}
+                  ref={this.webcam}
+                  screenshotFormat="image/jpeg"
+                  videoConstraints={videoConstraints}
+                />
+              </div>
+            ) : null}
+            {!!drawBox ? drawBox : null}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default withRouter(VideoInput);
