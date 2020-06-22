74aef7a721c22c4334b89dfa44cff23ededd9808
kaidornel <kai_dornel@hotmail.com>
2020-06-22 15:49:39 -0400

    removed comments

--- src/companyProfileComponents/CreateNewLessonContainer.js
+++ src/companyProfileComponents/CreateNewLessonContainer.js
@@ -15,40 +15,6 @@ class CreateNewLessonContainer extends React.Component{
             lessonsArray: []
         }
     }
-    // whenever add next lesson is clicked, reset string states. X
-    // send string states to redux store and have it replace old string states
-    // when clicking submit, add string states to lessons arrays
-    // addNextLesson = () => {
-        
-    //     let lessonName = this.state.lessonName // prevents bug with updatedLessonArray array
-    //     let updatedLessonsArray = [...this.state.lessonsArray, lessonName, this.state.lessonText, this.state.video]
-       
-        
-    //     this.setState({
-    //         lessonsArray: updatedLessonsArray
-    //     },() => {
-    //         if( this.state.video.length > 0 && !this.state.video.includes('youtube.com/embed') ){ // https://www.youtube.com/embed/nghuHvKLhJA
-    //             alert("Please use a valid youtube embed")
-    //         }else{
-    //         if( this.state.lessonName.length === 0 || this.state.lessonText.length === 0 ){
-    //             alert("All required lesson fields must be filled in")
-    //         }else{
-    //         let newNumLessons = [...this.state.numberOfLessons, 1]
-    //         this.setState({numberOfLessons: newNumLessons})
-
-
-
-    //         this.props.addLessonsToCourse(this.state.lessonsArray)
-    //         this.setState({
-    //             lessonName: "",
-    //             lessonText: "",
-    //             video: "",
-    //             lessonsArray: []
-    //         })
-    //         // alert("Lesson has been submitted. You may now create another lesson below.")
-    //         }
-    //     }})     
-    // }
 
     addNextLesson = () => {
         
