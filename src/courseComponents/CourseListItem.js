acbe1dc3f58ef265f42a902f83aec1ed88698c2a
kaidornel <kai_dornel@hotmail.com>
2020-07-23 13:43:03 -0400

    added props parameter and fixed object in test

--- src/courseComponents/CourseListItem.js
+++ src/courseComponents/CourseListItem.js
@@ -3,19 +3,19 @@ import { connect } from "react-redux";
 import { Card, Icon, Image, Grid } from 'semantic-ui-react'
 import { Link, Route } from "react-router-dom"
 
-const CourseListItem = () => {
+const CourseListItem = (props) => {
     return (
         <div className="course-card-div">
-            <Link to={`/course-list/${this.props.course.id}/`} >
+            <Link to={`/course-list/${props.course.id}/`} >
                 <Card.Group>
                     <Card className="course-card">
                         <div className="course-card-image-div">
-                            <Image classname="course-card-image" src={this.props.course.picture} />
+                            <Image classname="course-card-image" src={props.course.picture} />
                         </div>
                         <Card.Content>
-                            <h3 className="course-card-text course-card-info">{this.props.course.name}</h3>
-                            <h5 className="course-card-text course-card-info">{this.props.course.subject}</h5>
-                            <h5 className="course-card-text course-card-info-last">{this.props.course.company.name}</h5>
+                            <h3 className="course-card-text course-card-info">{props.course.name}</h3>
+                            <h5 className="course-card-text course-card-info">{props.course.subject}</h5>
+                            <h5 className="course-card-text course-card-info-last">{props.course.company.name}</h5>
                         </Card.Content>
                     </Card>
                 </Card.Group>
--- src/tests/CourseListItem.test.js
+++ src/tests/CourseListItem.test.js
@@ -2,32 +2,21 @@ import React from 'react';
 import {shallow} from 'enzyme';
 import CourseListItem from '../courseComponents/CourseListItem';
 
-const mockProps = {
-    id: 5000,
-    name: "Learn C",
-    image: "C Coding Picture",
-    subject: "Computer Science",
-    company_name: "MIT"
+const props = {
+    course: {
+        id: 5000,
+        name: "Learn C",
+        image: "C Coding Picture",
+        subject: "Computer Science",
+        company: {
+            name: "MIT"
+        }
+    }
 }
 
 describe('CourseListItem', () => {
-    // it('should render course image', () => {
-    //     expect(shallow())
-    // })
-
-    // it('should render course name', () => {
-        
-    // })
-
-    // it('should render course subject', () => {
-        
-    // })
-
-    // it('should render course company name', () => {
-        
-    // })
     it("should receive props", () => {
-        const wrapper = shallow(<CourseListItem {...mockProps}/>)
+        const wrapper = shallow(<CourseListItem {...props} />)
         expect(wrapper).toMatchSnapshot()
     })
 })
